93b44e4fc438dff57824cdf9d5d9e215f3093619
Mark Gile <markgile1107@gmail.com>
2023-09-28 14:59:00 +0800

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,20 @@
-<template></template>
+
+<script setup>
+import { ref } from 'vue';
+
+const inputText = ref('hello')
+const gender = ref('')
+
+</script>
+<template>
+    gender
+    <select v-model="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="others">Others</option>
+    </select>
+    <p v-if="gender == 'male'">
+    <h1>Test display</h1>
+    </p>
+    <p v-else>3</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/js/index.umd.js'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/js/index.umd.js'
 
 createApp(App).mount('#app')
