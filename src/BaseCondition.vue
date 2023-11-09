f2fa218a5e57b3ebedb48b38a21cd687ce59e549
Mark Gile <markgile1107@gmail.com>
2023-11-09 20:32:01 +0800

    add

---
+++ src/BaseCondition.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const inputText = ref('hello')
+const gender = ref('')
+
+</script>
+<template>
+    gender
+    <select v-model="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="others">Others</option>
+    </select>
+    <p v-if="gender == 'male'">
+    <h1>Test display</h1>
+    </p>
+    <p v-else>3</p>
+</template>
---
+++ src/BaseLoop.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+let numbers = ref([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+])
+console.log('test')
+</script>
+<template>
+    <p v-for="(number, index) in numbers">
+        <span v-if="number != 'c'">
+            text: {{ number }}-{{ index }}
+        </span>
+    </p>
+    <button @click="numbers.push('Hello')">Add</button>
+</template>
