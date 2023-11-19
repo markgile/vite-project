e817961279adc1c5d01f6e9b0bcf1663c74cc4ef
Mark Gile <markgile1107@gmail.com>
2023-11-19 08:55:28 +0800

    first

--- src/BaseCondition.vue
+++ src/BaseCondition.vue
@@ -1,19 +1,21 @@
-<script setup>
-import { ref } from 'vue';
-
-const inputText = ref('hello')
-const gender = ref('')
-
-</script>
 <template>
-    gender
-    <select v-model="gender">
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="others">Others</option>
-    </select>
-    <p v-if="gender == 'male'">
-    <h1>Test display</h1>
-    </p>
-    <p v-else>3</p>
+  <div>
+    <BaseCondition />
+  </div>
+
+  <div>
+    <BaseLoop />
+  </div>
 </template>
+
+<script>
+  import BaseLoop from './BaseLoop.vue';
+  import BaseCondition from './BaseCondition.vue';
+
+  export default {
+    components: {
+      BaseCondition,
+      BaseLoop,
+    },
+  };
+</script>
